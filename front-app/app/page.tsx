c1aec629cbd67c05a336b1c9545b33c65a687d64
mlvynhrl <91595222+mlvynhrl@users.noreply.github.com>
2025-07-15 11:51:46 +0200

    Added images to features section

--- front-app/app/page.tsx
+++ front-app/app/page.tsx
@@ -1,5 +1,5 @@
 import CallToAction from "@/components/call-to-action";
-import FeaturesSection from "@/components/features-8";
+import FeaturesSection from "@/components/features";
 import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 import WallOfLoveSection from "@/components/testimonials";
