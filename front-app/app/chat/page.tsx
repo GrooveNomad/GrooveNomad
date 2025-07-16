884213858ae458f27f57112832573758710840e1
mlvynhrl <91595222+mlvynhrl@users.noreply.github.com>
2025-07-16 15:54:27 +0200

    fix: client and serveur pb

--- front-app/app/chat/page.tsx
+++ front-app/app/chat/page.tsx
@@ -1,20 +1,17 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import ChatDemo from "@/components/chat";
+import { Suspense } from "react";
+import ClientOnlyWrapper from "@/components/ClientOnlyWrapper";
 
 export default function ChatPage() {
-  const searchParams = useSearchParams();
-  const initialMessage = searchParams.get("message");
-
   return (
     <div className="flex flex-col h-screen">
       <header className="sticky top-0 z-10 text-center bg-white border-b p-4">
-        <h1 className="text-2xl font-bold">Groove Nomad Chat</h1>
+        <h1 className="text-2xl font-bold">GrooveNomad chat</h1>
       </header>
 
       <main className="flex-1 overflow-hidden">
-        <ChatDemo initialMessage={initialMessage} />
+        <Suspense fallback={<div>Chargement du chat…</div>}>
+          <ClientOnlyWrapper />
+        </Suspense>
       </main>
     </div>
   );
